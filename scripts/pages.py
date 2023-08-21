5f162fe74f432c75b694836361974e10b7192893
Mark Ribeiro <40504056+MarkRibeiro@users.noreply.github.com>
2023-08-21 20:37:35 -0300

    Adiciona navegacao entre as paginas

--- scripts/pages.py
+++ scripts/pages.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
+client = MongoClient("localhost", 27017)
+db = client["mari_tcc"]
 
 @app.route("/")
-
 def start_page():
-    return
+    return redirect("/login")
+
+@app.route("/login")
+def login_page():
+    user = {"name":"Mari"}
+    db["users"].insert_one(user)
+    return "login"
+
+@app.route("/home")
+def home_page():
+    return render_template("home.html")
+
+app.run(port=5000, debug=True)
